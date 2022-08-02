7517891831a11a3afe0712cdb5ea8942a47e4450
dsaipe <d.f.saipe2@newcastle.ac.uk>
2022-08-02 11:37:29 +0100

    Using rtweet package to get twitter data

--- R/obtaining_data.R
+++ R/obtaining_data.R
@@ -1,3 +1,14 @@
+library(rtweet)
+
+# @jumping_uk timeline
+jr_timeline = get_timeline('jumping_uk', n = 2000)
+write_as_csv(jr_timeline, 'jr_timeline.csv')
+
+# #rstats timeline
+rstats = search_tweets('#rstats', n = 5000)
+write_as_csv(rstats, 'rstats_hashtag.csv')
+
+# @jumping_uk retweeters
 morning = jr_timeline %>%
   filter(hour_posted <= 13)
 afternoon = jr_timeline %>%
@@ -15,4 +26,7 @@ for(i in 1:nrow(afternoon)){
 }
 
 morning_retweeters = lookup_users(am_rt)
+write_as_csv(morning_retweeters, 'morning_retweeters.csv')
+
 afternoon_retweeters = lookup_users(pm_rt)
+write_as_csv(afternoon_retweeters, 'afternoon_retweeters.csv')
